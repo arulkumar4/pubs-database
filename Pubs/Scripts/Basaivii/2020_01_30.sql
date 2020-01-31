08d2a1263be557f91ab0dc879d26ac213dbfc313
basivii <kelam.basavaiah@kanini.com>
2020-01-31 19:08:08 +0530

    End of Day

--- Pubs/Scripts/Basaivii/2020_01_30.sql
+++ Pubs/Scripts/Basaivii/2020_01_30.sql
@@ -14,6 +14,8 @@ right (REVERSE(right (TestValue,len(Testvalue)-charindex('.',TestValue,charindex
 				)
 FROM @TestTable
 
+SELECT FNNAME(tE) FROM 
+
 -- link
 --------  "https://dbfiddle.uk/?rdbms=sqlserver_2019&fiddle=afff822c2f777dcd3e622c4d9a2851a2"
 
@@ -30,3 +32,38 @@ SELECT parsename(TestValue,2) From @TestTable
 
 --------link 
 ----------------  "https://dbfiddle.uk/?rdbms=sqlserver_2019&fiddle=9252e66acf4239b656655b241b779ad2"
+
+
+
+--scalar Valued Function
+go
+
+CREATE FUNCTION addition (@fristNum int ,@secondNum int )
+RETURNS @thirdValue TABLE (FilteredValue varchar(20))
+AS 
+BEGIN
+INSERT INTO @thirdValue VALUES ()
+RETURN 
+END
+
+
+
+
+----
+CREATE FUNCTION FILTERED_IP(@Value varchar)
+RETURNS @thirdValue TABLE (FilteredValue varchar(20))
+AS 
+BEGIN
+INSERT INTO @thirdValue VALUES (@Value)
+RETURN ;
+END;
+ 
+ DECLARE @table1 table (alue varchar)
+set @table1=FILTERED_IP('2')
+ SELECT * FROM @table1
+
+  
+CREATE TABLE TestTable  (TestValue VARCHAR (20));
+
+INSERT INTO TestTable (TestValue) VALUES
+('192.256.1.89'), ('192.255.091.900'), ('192.11.34.345');
