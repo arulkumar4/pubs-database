4c238d72cde6be533b64ec58fd03f4d8812f403b
basivii <kelam.basavaiah@kanini.com>
2020-02-06 18:52:01 +0530

    Predefined functions Task

---
+++ Pubs/Scripts/Basaivii/2020_02_06.sql
@@ -0,0 +1,52 @@
+﻿Select * from tb_emp_id
+
+Select * from stores
+select * from titles
+
+--Rank
+
+Select  RANK() OVER(ORDER BY city DESC) num ,city FROM tb_emp_id
+
+Select  RANK() OVER(ORDER BY city ASC) num ,city FROM tb_emp_id
+
+
+
+Select  RANK() OVER(ORDER BY state ASC) num ,city , stor_id FROM stores
+
+Select  RANK() OVER(ORDER BY state DESC) num ,city,stor_id FROM stores
+
+--row num
+
+Select  ROW_NUMBER() OVER(ORDER BY state DESC) num ,city,stor_id FROM stores
+
+Select  ROW_NUMBER() OVER(ORDER BY state ASC) num ,city , stor_id FROM stores
+
+
+--all in one
+
+
+
+Select  royalty as order_by ,
+	   RANK() OVER(  ORDER BY royalty DESC) RANKK ,
+	   Dense_rank() OVER( ORDER BY royalty DESC) DENSE_RANKK ,
+	   ROW_NUMBER() OVER( ORDER BY royalty DESC) ROW_NUM  FROM titles
+
+
+Select  royalty as order_by,  type as partistion,
+		 RANK() OVER(PARTITION BY type ORDER BY royalty DESC) RANKK 
+		,Dense_rank() OVER(PARTITION BY type ORDER BY royalty DESC) DENSE_RANKK ,
+	   ROW_NUMBER() OVER(PARTITION BY type ORDER BY royalty DESC) ROW_NUM
+	   FROM titles
+
+ -- cast,coalesce,scope_identity(),ltrim,rtrim
+
+SELECT CAST(22.2 AS INT)
+
+SELECT COALESCE(NULL,1,NULL)
+
+SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY];  
+
+SELECT LTRIM('   ISDCBUHSDB')
+SELECT RTRIM('   ISDCBUHSDB')
+SELECT LTRIM('   ISDCBUHSDB     ')
+SELECT RTRIM('   ISDCBUHSDB     ')
