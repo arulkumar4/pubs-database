ebfa8dcc836ce6631d400f6c8d3bf8d13af35e06
GayathriMukesh <57354099+GayathriMukesh@users.noreply.github.com>
2020-02-12 11:49:46 +0530

    tasks(10&11 feb 2020)

---
+++ Pubs/Scripts/Gayathri/2020_02_10.sql
@@ -0,0 +1,72 @@
+﻿--STUFF 
+SELECT STUFF
+(
+	'Welcome To GISbiz Technologies', 12, 20, 'Kanini Softwares'
+) AS New_String
+
+--DISTINCT 
+SELECT City
+FROM [10th_StudentDetails].[Teachers]
+
+--DML Trigger
+CREATE TRIGGER [10th_StudentDetails].[TeachersRecord]
+ON
+[10th_StudentDetails].[Teachers]
+INSTEAD OF DELETE 
+--FOR UPDATE
+--AFTER INSERT
+AS 
+BEGIN
+PRINT 'You DONT have permission to delete from that table'
+END
+
+DELETE FROM [10th_StudentDetails].[Teachers] WHERE City='Chennai'
+
+--DDL TRIGGER
+CREATE TRIGGER DropTable
+ON 
+DATABASE
+FOR DROP_TABLE
+AS
+BEGIN
+Print 'Table dropped Successfully!!!'
+END
+
+DROP TABLE Employee
+
+--Cursor to PRINT Teachers ID & Name
+
+ALTER PROCEDURE [10th_StudentDetails].[TeachersDetails]
+AS
+BEGIN
+    DECLARE 
+    @TeacherID INT,
+    @Teachername VARCHAR(MAX)
+    DECLARE cursor_teacher CURSOR
+    FOR SELECT 
+            TeachersID, 
+            TeachersName 
+        FROM 
+            [10th_StudentDetails].[Teachers]
+ 
+    OPEN cursor_teacher
+ 
+    FETCH NEXT FROM cursor_teacher INTO 
+        @TeacherID, 
+        @Teachername
+ 
+    WHILE @@FETCH_STATUS = 0
+        BEGIN
+            PRINT CAST(@TeacherID AS VARCHAR) +' - '+ @Teachername 
+            FETCH NEXT FROM cursor_teacher INTO 
+                @TeacherID, 
+                @Teachername
+        END
+ 
+    CLOSE cursor_teacher
+ 
+    DEALLOCATE cursor_teacher
+END
+
+EXEC [10th_StudentDetails].[TeachersDetails]
+
